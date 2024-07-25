644f2bf7b6d3c6554c4071d9e078cb431e753cbf
ntinos <120028619+Ntinos47@users.noreply.github.com>
2024-07-25 12:46:41 +0300

    small changes

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+const boardcastBtn = document.getElementById('btn-boardcast');
+const publishingBtn = document.getElementById('btn-publishing');
+const contentBtn = document.getElementById('btn-content');
+const boardcastContent = document.getElementById('1st');
+const publishingContent = document.getElementById('2nd');
+const contentContent = document.getElementById('3rd');
+
 document.addEventListener('DOMContentLoaded', function() {
-    const boardcastBtn = document.getElementById('btn-boardcast');
-    const publishingBtn = document.getElementById('btn-publishing');
-    const contentBtn = document.getElementById('btn-content');
-    const boardcastContent = document.getElementById('1st');
-    const publishingContent = document.getElementById('2nd');
-    const contentContent = document.getElementById('3rd');
-
-
-
     boardcastBtn.addEventListener('click', function() {
         boardcastContent.style.display = 'block';
         publishingContent.style.display = 'none';
@@ -52,4 +50,35 @@ document.addEventListener('DOMContentLoaded', function() {
     footerContent.addEventListener('click', function() {
         contentBtn.click();
     });
+});
+
+
+
+const boardPage = document.getElementById('page-sec1');
+const pubPage = document.getElementById('page-sec2');
+const conPage = document.getElementById('page-sec3');
+
+boardPage.addEventListener('click', function() {
+    boardcastContent.style.display = 'block';
+    publishingContent.style.display = 'none';
+    contentContent.style.display = 'none';
+    boardcastBtn.classList.add('active');
+    publishingBtn.classList.remove('active');
+    contentBtn.classList.remove('active');
+});
+pubPage.addEventListener('click', function() {
+    boardcastContent.style.display = 'none';
+    publishingContent.style.display = 'block';
+    contentContent.style.display = 'none';
+    boardcastBtn.classList.remove('active');
+    publishingBtn.classList.add('active');
+    contentBtn.classList.remove('active');
+});
+pubPage.addEventListener('click', function() {
+    boardcastContent.style.display = 'none';
+    publishingContent.style.display = 'none';
+    contentContent.style.display = 'block';
+    boardcastBtn.classList.remove('active');
+    publishingBtn.classList.remove('active');
+    contentBtn.classList.add('active');
 });
